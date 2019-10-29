95974c91cbebfa6f45fe88b9ec1b6178dcc598a9
Bluecoast <51225257+Bluecoast-Heiko@users.noreply.github.com>
2019-10-29 15:03:35 +1000

    re-upload

---
+++ data/StudyArea_500m_3.js
@@ -0,0 +1,8 @@
+var json_StudyArea_500m_3 = {
+"type": "FeatureCollection",
+"name": "StudyArea_500m_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 153.608677201415645, -28.636541197853148 ], [ 153.618774630558136, -28.639098394285284 ], [ 153.617791862085454, -28.642182084501588 ], [ 153.607675296392557, -28.63960969244097 ], [ 153.608677201415645, -28.636541197853148 ] ] ] } }
+]
+}
